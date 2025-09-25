4b6f2bf528246a812e29ac3ece934c8ba1a327fe
Zain Latif <zainlatif702@gmail.com>
2025-09-25 13:13:04 +0500

    first commit of Data Analyst internship

---
+++ Python Classs/intro.py
@@ -0,0 +1,19 @@
+# Data Structures in Python
+
+# a = True
+# print(10 * 1 + a)
+
+# str = "hello"
+# print(str * 3)
+# li = [1,2,3] * 4
+# print(li)
+# tup = (1,2,3) * 4
+# print(li)
+
+# mutable and immutable
+
+# li = (1,2,3)
+# li[1] = 10
+# print(li)
+
+# _________________________________
